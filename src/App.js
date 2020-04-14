8b438d0387cf5fe25a3d7bab58ee9cdc9b340c34
Rohan Joshi <joshi.rohan09@gmail.com>
2020-04-13 23:56:41 -0400

    Replace with React hooks.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Person from './Person/Person';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  // state is managed from inside of the component
-  state = {
+const App = props => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name:"Rohan", age: 25},
       { name: "Chris", age: 57}
     ],
-  }
+  },);
 
-  handleSwitchName = () => {
-    console.log("Was Clicked");
+  const handleSwitchName = () => {
+    // this.state.persons[0].name = "Dhoshya"
+    // This is the wrong way, use setState()
+    setPersonsState({
+      persons: [
+        { name:"Dhoshya", age: 25},
+        { name: "Chris", age: 48}
+      ],
+    }); 
   };
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          This is really  working.
-        </p>
-        <button onClick={this.handleSwitchName}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}>
-          My Hobbies: Racing
-        </Person>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <p className="App-intro">
+        This is really  working.
+      </p>
+      <button onClick={handleSwitchName}>Switch Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}>
+        My Hobbies: Racing
+      </Person>
+    </div>
+  );
 }
 
 export default App;
+
+// state is managed from inside of the component
+// if state or props are changed, react re-renders the component.
+// state = {
+//
+// }
